260a91bc536d4823b8e9255f4586b05cfe0f3c7b
Dufyz <guilhermeiagothomaz@hotmail.com>
2024-12-22 16:02:35 -0300

    feat: Implement WebSocket server with room management and event broadcasting

---
+++ apps/server/internal/interfaces/rest/routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"server/internal/interfaces/rest/controller"
+
+	"github.com/labstack/echo/v4"
+)
+
+func UseRoutes(e *echo.Echo) {
+	api := e.Group("/api")
+
+	WebsocketController := controller.NewWebsocketController()
+	api.GET("/web-socket", WebsocketController.HandleConnection)
+}
